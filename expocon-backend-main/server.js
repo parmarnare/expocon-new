517101cd2263267587e1c7c27b71a6bba66cab73
narendra <nareparmar@gmail.com>
2024-12-14 13:22:31 +0530

    backend changes

--- expocon-backend-main/server.js
+++ expocon-backend-main/server.js
@@ -25,7 +25,6 @@ app.use(cors(
   }
 ));
 app.use(express.json());
-
 app.use("/api/v1/auth", userRouter);
 app.use("/api/v1/badge", badgeRouter);
 app.use("/api/v1/certificate", certificateRouter);
