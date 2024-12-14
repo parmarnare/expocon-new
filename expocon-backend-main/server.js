458b62d5f94b2f8374e50134b8f027ef53bcd61a
narendra <nareparmar@gmail.com>
2024-12-14 12:54:07 +0530

    config changes

--- expocon-backend-main/server.js
+++ expocon-backend-main/server.js
@@ -19,7 +19,7 @@ dotenv.config();
 const app = express();
 app.use(cors(
   {
-    origin: ["https://expocon-nare.vercel.app"],
+    origin: ["https://expocon-nare-bdcq.vercel.app/"],
     method: ["POST", "GET"],
     credentials: true
   }
