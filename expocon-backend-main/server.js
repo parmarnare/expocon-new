f131ea2c5f2afeb067dd03ef32abe714d8e4d470
narendra <nareparmar@gmail.com>
2024-12-15 14:09:03 +0530

    url change11s1

--- expocon-backend-main/server.js
+++ expocon-backend-main/server.js
@@ -17,13 +17,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(cors(
-  {
-    origin:  "http://localhost:3000",
-    method: ["GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],
-    credentials: true
-  }
-));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/auth", userRouter);
